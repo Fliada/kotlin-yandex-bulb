0bb2f400af154bd52a251cfd6fbe7db576282ce0
Alina <wbrflf3301@outlook.com>
2024-01-09 00:29:26 +0500

    the methods are implemented in MainRepositoryImpl

--- app/src/main/java/com/example/kotlin_yandex_bulb/data/repository/MainRepository.kt
+++ app/src/main/java/com/example/kotlin_yandex_bulb/data/repository/MainRepository.kt
@@ -1,9 +1,28 @@
 package com.example.kotlin_yandex_bulb.data.repository
 
+import com.example.kotlin_yandex_bulb.data.BrightnessData
 import com.example.kotlin_yandex_bulb.data.ColorData
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface MainRepository {
     suspend fun getColors(): Result<List<ColorData>?>
     suspend fun setColor(color: String): Result<Unit>
+
+    suspend fun getCurrentColor(): Result<ColorData?>
+
+    suspend fun getBrightnessLevels(): Result<BrightnessData?>
+
+    suspend fun getCurrentBrightnessLevel(): Result<Int?>
+
+    suspend fun setBrightnessLevel(level: Int): Result<Unit>
+
+    suspend fun turnOn(): Result<Unit>
+
+    suspend fun turnOff(): Result<Unit>
+
+    suspend fun getCurrentState(): Result<Boolean?>
 }
 
--- app/src/main/java/com/example/kotlin_yandex_bulb/data/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/example/kotlin_yandex_bulb/data/repository/MainRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin_yandex_bulb.data.repository
 
 import android.util.Log
+import com.example.kotlin_yandex_bulb.data.BrightnessData
 import com.example.kotlin_yandex_bulb.data.ColorData
 import com.example.kotlin_yandex_bulb.data.api.MainService
 import retrofit2.HttpException
@@ -43,6 +44,111 @@ class MainRepositoryImpl @Inject constructor(
             onFailure = {
                 Log.d("MainRepositoryImpl", "${it.stackTrace[1]}")
 
+                return Result.failure(it)
+            }
+        )
+    }
+
+    override suspend fun getCurrentColor(): Result<ColorData?> {
+        kotlin.runCatching {
+            service.getCurrentColor()
+        }.fold(
+            onSuccess = {
+                return if (it.isSuccessful)
+                    Result.success(it.body())
+                else Result.failure(HttpException(it))
+            },
+            onFailure = {
+                return Result.failure(it)
+            }
+        )
+    }
+
+    override suspend fun getBrightnessLevels(): Result<BrightnessData?> {
+        kotlin.runCatching {
+            service.getBrightnessLevels()
+        }.fold(
+            onSuccess = {
+                return if (it.isSuccessful)
+                    Result.success(it.body())
+                else Result.failure(HttpException(it))
+            },
+            onFailure = {
+                return Result.failure(it)
+            }
+        )
+    }
+
+    override suspend fun getCurrentBrightnessLevel(): Result<Int?> {
+        kotlin.runCatching {
+            service.getCurrentBrightnessLevel()
+        }.fold(
+            onSuccess = {
+                return if (it.isSuccessful)
+                    Result.success(it.body())
+                else Result.failure(HttpException(it))
+            },
+            onFailure = {
+                return Result.failure(it)
+            }
+        )
+    }
+
+    override suspend fun setBrightnessLevel(level: Int): Result<Unit> {
+        kotlin.runCatching {
+            service.setBrightnessLevel(level)
+        }.fold(
+            onSuccess = {
+                return if (it.isSuccessful)
+                    Result.success(Unit)
+                else Result.failure(HttpException(it))
+            },
+            onFailure = {
+                return Result.failure(it)
+            }
+        )
+    }
+
+    override suspend fun turnOn(): Result<Unit> {
+        kotlin.runCatching {
+            service.turnOn()
+        }.fold(
+            onSuccess = {
+                return if (it.isSuccessful)
+                    Result.success(Unit)
+                else Result.failure(HttpException(it))
+            },
+            onFailure = {
+                return Result.failure(it)
+            }
+        )
+    }
+
+    override suspend fun turnOff(): Result<Unit> {
+        kotlin.runCatching {
+            service.turnOff()
+        }.fold(
+            onSuccess = {
+                return if (it.isSuccessful)
+                    Result.success(Unit)
+                else Result.failure(HttpException(it))
+            },
+            onFailure = {
+                return Result.failure(it)
+            }
+        )
+    }
+
+    override suspend fun getCurrentState(): Result<Boolean?> {
+        kotlin.runCatching {
+            service.getCurrentState()
+        }.fold(
+            onSuccess = {
+                return if (it.isSuccessful)
+                    Result.success(it.body())
+                else Result.failure(HttpException(it))
+            },
+            onFailure = {
                 return Result.failure(it)
             }
         )
